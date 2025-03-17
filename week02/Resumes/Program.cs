7f7aa8f1605bdc29834637f0a72ecb1b5c33fdff
Helamã VPC <helamacorrea@gmail.com>
2025-03-17 12:00:32 -0300

    Added Resumes Class and Job Class

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -1,9 +1,37 @@
 using System;
+using System.ComponentModel;
+using System.Security.Cryptography.X509Certificates;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        Job job1 = new Job();
+        job1._jobTitle = "Software Engineer";
+        job1._companyName = "Google";
+        job1._startYear = 2025;
+        job1._endYear = 2027;
+
+        Job job2 = new Job();
+        job2._jobTitle = "Engineer";
+        job2._companyName = "Tesla";
+        job2._startYear = 2025;
+        job2._endYear = 2028;
+        //Console.WriteLine(job1._companyName);
+        //Console.WriteLine(job2._companyName);
+        //job1.Display();
+        //job2.Display();
+
+        Resumes resume1 = new Resumes();
+        resume1._name = "Gabrielly";
+        resume1._jobs.Add(job1);
+        resume1._jobs.Add(job2);
+
+        resume1.Display();
+
+
+
+
+
     }
 }
