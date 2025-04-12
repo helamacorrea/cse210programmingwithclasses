811ce224c844c45d0b85f3fe0ef503909c802ec8
Helamã VPC <helamacorrea@gmail.com>
2025-04-11 21:14:58 -0300

    Shapes  program first version

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -4,6 +4,25 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Shapes Project.");
+        Square square1 = new Square("white", 30);
+        Console.WriteLine(square1.GetArea());
+        Rectangle rec1 = new Rectangle("purple", 20, 10);
+        Console.WriteLine(rec1.GetArea());
+        Circle cir1 = new Circle("black", 4.5);
+        Console.WriteLine(cir1.GetArea());
+
+
+        List<Shapes> shapesList = new List<Shapes>();
+        shapesList.Add(square1);
+        shapesList.Add(rec1);
+        shapesList.Add(cir1);
+
+        foreach (Shapes shape in shapesList)
+        {
+            Console.WriteLine(shape.GetColor());
+            Console.WriteLine(shape.GetArea());
+        }
+        
+        
     }
 }
